5c7f2c9826dd7e8f5835f071bd37b8b46c8867c9
Paul <drcmda@gmail.com>
2016-11-13 15:21:27 +0100

    cleanup

--- components/StackedGrid.js
+++ components/StackedGrid.js
@@ -23,23 +23,19 @@ export default class StackedGrid extends React.Component {
             let left = 100 * index;
             let offset = (index + 1) * margin;
             let top = columnHeights[index] + margin;
+            let style = {
+                position: 'absolute',
+                transform: `translate3d(${margin ? `calc(${left}% + ${offset}px)` : `${left}%`},${top}px,0)`,
+                width: `${margin ? `calc(${width}% - ${margin}px)` : `${width}%`}`,
+                height: child.props.height
+            };
             columnHeights[index] += child.props.height + margin;
-            return (
-                <div key={child.key} style={{
-                    position: 'absolute',
-                    transform: `translate3d(${margin ? `calc(${left}% + ${offset}px)` : `${left}%`},${top}px,0)`,
-                    width: `${margin ? `calc(${width}% - ${margin}px)` : `${width}%`}`,
-                    height: child.props.height }}>
-                    {child}
-                </div>
-            )
+            return <div key={child.key} style={style}>{child}</div>;
         });
         let height = Math.max(...columnHeights);
         return (
             <div style={{ padding: `0 ${margin}px ${margin}px 0`}}>
-                <div style={{ position: 'relative', width: '100%', height }}>
-                    {grid}
-                </div>
+                <div style={{ position: 'relative', width: '100%', height }}>{grid}</div>
             </div>
         );
     }
